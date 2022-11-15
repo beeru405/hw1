ee6039b1729a942a23b95689e7e09993e3268301
dude <dude405@gmail.com>
2022-11-15 11:35:27 +0530

    all files

---
+++ src/helloworld.java
@@ -0,0 +1,7 @@
+package com.aks;
+
+public class Test {
+	public static Strting m1() {
+		return "Hello World";
+}
+}
---
+++ test/helloworld.java
@@ -0,0 +1,15 @@
+package com.aks;
+
+public class Test1 {
+	public statis void main(String[] args) {
+		String results = Test.m1();
+		System.out.println(results);
+		String[] res = results.split(" ");
+		if (res[0],equals("Hello")) {
+			System.out.println("Test Passed");
+		} else {
+			System.out.println("Test Failed");
+		}
+	}
+}
+
